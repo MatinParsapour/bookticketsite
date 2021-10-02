12f34893fd50e8b448e949cc7f7202c43cf37c18
Matin <matin.parsapour.iam@gmail.com>
2021-10-02 12:52:45 +0330

    add CEOEnterServlet and CEOCodeValidation for ceo to enter

---
+++ src/main/java/servlets/ceoservlets/CEOCodeValidation.java
@@ -0,0 +1,31 @@
+package servlets.ceoservlets;
+
+import domain.CEO;
+import util.ApplicationContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class CEOCodeValidation extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        HttpSession session = req.getSession(false);
+        String ceoCode = req.getParameter("ceoCode");
+        session.setAttribute("ceoCode",ceoCode);
+        PrintWriter out = resp.getWriter();
+        long ceoCodeLong = Long.parseLong((String) session.getAttribute("ceoCode"));
+        CEO ceo = ApplicationContext.getCEOService().checkCEO(ceoCodeLong);
+        if(ceo != null){
+            out.println("Welcome : " + ceo.getFirstName() + " " + ceo.getLastName());
+        }else{
+            out.println("The code is wrong");
+
+        }
+    }
+}
---
+++ src/main/java/servlets/ceoservlets/CEOEnterServlet.java
@@ -0,0 +1,29 @@
+package servlets.ceoservlets;
+
+import util.ApplicationContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class CEOEnterServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<body style=\"background-image: url('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.istockphoto.com%2Fphotos%2Fceo-text-on-white-background-picture-id481914276&f=1&nofb=1'); background-size: cover\">");
+        out.println("<form action=\"ceoValidation\">");
+        out.println("<div style=\"text-align: center\">");
+        out.println("<input type=\"text\" name='ceoCode' onchange=\"this.value = parseInt(this.value);\">");
+        out.println("<br>");
+        out.println("<br>");
+        out.println("<input type=\"submit\" value=\"Accept\">");
+        out.println("</div>");
+        out.println("</form>");
+        out.println("</body>");
+    }
+}
