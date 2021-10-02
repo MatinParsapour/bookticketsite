e5c5c94e69d19ec6e27caf03b95dd232a90d5a01
Matin <matin.parsapour.iam@gmail.com>
2021-10-02 08:09:14 +0330

    add session to the project

--- src/main/java/WebSite.java
+++ src/main/java/WebSite.java
@@ -2,12 +2,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class WebSite extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
         out.println("<form action=\"ceo\">");
