ad4e2f6db9028519a3556876b654c6abfcac6ba2
Matin <matin.parsapour.iam@gmail.com>
2021-10-02 08:04:50 +0330

    Update main of project

--- src/main/java/WebSite.java
+++ src/main/java/WebSite.java
@@ -1,5 +1,29 @@
-public class WebSite {
-    public static void main(String[] args) {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+public class WebSite extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<form action=\"ceo\">");
+        out.println("<div style=\"text-align: center\">");
+        out.println("<input type=\"submit\" value=\"CEO\" style=\"width: 200px; height: 50px\">");
+        out.println("</div>");
+        out.println("</form>");
+        out.println("<form action=\"employee\">");
+        out.println("<div style=\"text-align: center\">");
+        out.println("<input type=\"submit\" value=\"employee\" style=\"width: 200px; height: 50px\">");
+        out.println("</div>");
+        out.println("</form>");
+        out.println("<form action=\"customer\">");
+        out.println("<div style=\"text-align: center\">");
+        out.println("<input type=\"submit\" value=\"customer\" style=\"width: 200px; height: 50px\">");
+        out.println("</div>");
+        out.println("</form>");
     }
 }
