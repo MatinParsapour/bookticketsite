d5f4773f240936b69057ff5528e8f29d4b8ea950
Matin <matin.parsapour.iam@gmail.com>
2021-10-05 16:33:19 +0330

    add a servlet for customers to delete their cards

---
+++ src/main/java/servlets/creditcardservlets/DeleteCard.java
@@ -0,0 +1,22 @@
+package servlets.creditcardservlets;
+
+import domain.CreditCard;
+import util.ApplicationContext;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class DeleteCard extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       long cardId = Long.parseLong(req.getParameter("creditCard"));
+        CreditCard creditCard = ApplicationContext.getCreditCardService().findById(cardId);
+        ApplicationContext.getCreditCardService().delete(creditCard);
+        RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/jsp/cards/CustomerCreditCards.jsp");
+        rd.forward(req,resp);
+    }
+}
