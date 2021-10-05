17fe568b665f3364cf0f9fc99e7e479dcdcbd052
Matin <matin.parsapour.iam@gmail.com>
2021-10-05 13:09:46 +0330

    create a method when customer enter card number check if card number is valid

---
+++ src/main/java/servlets/creditcardservlets/CardValidationServlet.java
@@ -0,0 +1,45 @@
+package servlets.creditcardservlets;
+
+import domain.CreditCard;
+import domain.Customer;
+import lombok.SneakyThrows;
+import util.ApplicationContext;
+import util.SecurityUser;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class CardValidationServlet extends HttpServlet {
+    @SneakyThrows
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        long cardNumber = Long.parseLong(req.getParameter("customerCardNumber"));
+        CreditCard creditCard = ApplicationContext.getCreditCardService().findDoplicateCardByCardNumber(cardNumber);
+        if(creditCard == null){
+            int cvv2 = Integer.parseInt(req.getParameter("customerCardCVV2"));
+            Date expirationDate = new SimpleDateFormat("yyyy-mm-dd").parse(req.getParameter("customerCardExpirationDate"));
+            int secondPassword = Integer.parseInt(req.getParameter("customerCardPassword"));
+            CreditCard newCreditCard = new CreditCard(cardNumber,secondPassword,expirationDate,cvv2);
+            newCreditCard = ApplicationContext.getCreditCardService().createOrUpdate(newCreditCard);
+            newCreditCard.setCustomer(SecurityUser.getCustomer());
+            newCreditCard = ApplicationContext.getCreditCardService().createOrUpdate(newCreditCard);
+            Customer customer = SecurityUser.getCustomer();
+            List<CreditCard> creditCardList = customer.getCreditCards();
+            creditCardList.add(newCreditCard);
+            ApplicationContext.getCustomerService().createOrUpdate(customer);
+            RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/jsp/customerjsp/CustomerMainMenu.jsp");
+            rd.forward(req,resp);
+        }else{
+            out.println("The card number is wrong");
+        }
+    }
+}
