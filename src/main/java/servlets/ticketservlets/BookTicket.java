06cbe550fb49879b41f2380859123e641a785752
Matin <matin.parsapour.iam@gmail.com>
2021-10-07 08:58:10 +0330

    create a servlet for customers to book a ticket they want

---
+++ src/main/java/servlets/ticketservlets/BookTicket.java
@@ -0,0 +1,34 @@
+package servlets.ticketservlets;
+
+import domain.Customer;
+import domain.History;
+import domain.Ticket;
+import util.ApplicationContext;
+import util.SecurityUser;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class BookTicket extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        long ticketId = Long.parseLong(req.getParameter("ticket"));
+        int numberOfTicket = Integer.parseInt(req.getParameter("numberOfPassengers"));
+        Ticket ticket = ApplicationContext.getTicketServiceImpl().findById(ticketId);
+        int newNumberOfPassenger = ticket.getNumberOfPassengers() - numberOfTicket;
+        ticket.setNumberOfPassengers(newNumberOfPassenger);
+        ApplicationContext.getTicketServiceImpl().createOrUpdate(ticket);
+        Customer customer = SecurityUser.getCustomer();
+        customer.getTickets().add(ticket);
+        ApplicationContext.getCustomerService().createOrUpdate(customer);
+        History history = new History(customer,ticket,numberOfTicket,true,false);
+        ApplicationContext.getHistoryServiceImpl().createOrUpdate(history);
+        RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/jsp/tickets/AvailableTickets.jsp");
+        rd.forward(req,resp);
+    }
+}
