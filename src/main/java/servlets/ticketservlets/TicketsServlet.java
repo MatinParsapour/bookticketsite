a39bba2de2417fae9df3df5d76316f579bd09d77
Matin <matin.parsapour.iam@gmail.com>
2021-10-07 20:44:07 +0330

    update TicketsServlet that final moments tickets have 50 percent off

--- src/main/java/servlets/ticketservlets/TicketsServlet.java
+++ src/main/java/servlets/ticketservlets/TicketsServlet.java
@@ -1,15 +1,27 @@
 package servlets.ticketservlets;
 
+import domain.Ticket;
+import util.ApplicationContext;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class TicketsServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Ticket> oneHourLastTickets = ApplicationContext.getTicketServiceImpl().getFinalMomentsTickets();
+        if(oneHourLastTickets.size() != 0){
+            for(Ticket ticket : oneHourLastTickets){
+                double newPrice = ticket.getAmount() / 2;
+                ticket.setAmount(newPrice);
+                ApplicationContext.getTicketServiceImpl().createOrUpdate(ticket);
+            }
+        }
         RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/jsp/tickets/Tickets.jsp");
         rd.forward(req, resp);
     }
